a1795670ae26f0f9161bdc0994c0aa155ede8f40
Jonpaul Wright <jonpaul.wright@gmail.com>
2023-10-28 19:33:07 -0600

    first commit

---
+++ constant.py
@@ -0,0 +1,30 @@
+info = {
+   "Name": "Jonpaul",
+   "Full_Name": "Jonpaul Wright",
+   "Intro": "Data Scientist and Engineer",
+   "About":"Howdy! I'm a curious and motivated data practitioner with experience in multiple domains and projects.  I enjoy working on all aspects of the full data stack and enjoy solving problems and optimizing (and automating) solutions as well as managing a team of skilled teammates to achieve real results.",
+#    "Tableau":"https://public.tableau.com/app/profile/vickytc/viz/SunnybrookTeam/Overview",
+#    "Medium":"https://medium.com/@vicky-note/about",
+   "City":"Denver, CO",
+   "Photo": """<a href=\"https://www.linkedin.com/in/jp-wright/\"><img src=\"https://jp-resumes.s3.amazonaws.com/jpgalvbnw.jpg" width=\"200\"   alt=\"Profile\" title=\"Profile\"></a>""",
+   "Email": "jonpaul.wright@icloud.com"
+}
+## github avatar
+# https://avatars.githubusercontent.com/u/14065301?v=4
+
+# embed_rss= {
+#     'rss':"""<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
+#         data-rss="https://medium.com/feed/@vicky-note"
+#         data-maxcols="3" 
+#         data-layout="grid"
+#         data-poststyle="inline" 
+#         data-readmore="Read the rest" 
+#         data-buttonclass="btn btn-primary" 
+#         data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""
+# }
+
+endorsements = {
+    "img1": "https://www.dropbox.com/scl/fi/wyel17y4eb34k5wla7oyn/jpw_endorsement_bvh.jpg?rlkey=s4ehfu8ggjol7gxwuyyngxihg&dl=0",
+    "img2": "https://www.dropbox.com/scl/fi/q05qh4zvqv9jyl8kg6q4u/jpw_endorsement_eg.jpg?rlkey=ypbn6ot208zuzxwhe1gki9xaj&dl=0",
+    "img3": "https://www.dropbox.com/scl/fi/9ia1vclypa4wlwhnzrpxa/jpw_endorsement_fp.jpg?rlkey=k8ni4aqqpw0qqind679c86fbr&dl=0"
+}
